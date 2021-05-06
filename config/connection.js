16da7d0eaa20a902e9022f301f2d9a13b3d573f7
Wayne Edwards <waedwards42@yahoo.com>
2021-05-05 22:47:55 -0700

    fixed syntax errors

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-require('dotenv'), config();
+require('dotenv').config();
 
 // create connection to our database, pass in your MySQL information for username and password
 const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
